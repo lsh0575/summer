a99087ae554386f5aebb42fdaf8276f9e61d175b
hyeeun <gpdms7087@gmail.com>
2023-06-26 15:24:23 +0900

    hyeeun - third commit

--- src/main/java/com/summer/dao/TestDao.java
+++
@@ -1,6 +0,0 @@
-package com.summer.dao;
-
-@MyMapper
-public interface TestDao {
-	public String readTime();
-}
--- src/main/java/com/summer/service/TestService.java
+++
@@ -1,5 +0,0 @@
-package com.summer.service;
-
-public interface TestService {
-	public String readTime();
-}
